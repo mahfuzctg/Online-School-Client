5781987953ac9ac10766ff96289f88c36efceabb
Mahfuz <aamahfuz.pro@gmail.com>
2023-06-12 23:28:56 +0600

    added something on popular classes

--- src/Components/PopularClassSub/PopularClassSub.jsx
+++ src/Components/PopularClassSub/PopularClassSub.jsx
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import useCarts from "../../Hooks/useCarts";
+import { AuthContext } from "../../Providers/AuthProviders";
 
 const PopularClassSub = ({ item }) => {
-  const { Name, Image, Instructor_name, Available_seats, Price } = item;
+  const { Name, Image, Instructor_name, Available_seats, Price, _id } = item;
+  const { user } = useContext(AuthContext);
+  const [, refetch] = useCarts();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const handleSelect = (cla) => {
+    console.log(cla);
+    if (user && user.email) {
+      const selectClass = {
+        selectClassId: _id,
+        Name,
+        Image,
+        Price,
+        email: user.email,
+      };
+      fetch("http://localhost:5000/carts", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(selectClass),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.insertedId) {
+            refetch();
+            //
+            Swal.fire("Successfully selected.");
+          }
+        });
+    } else {
+      Swal.fire({
+        title: "Please login before selecting.",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Login now",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          navigate("/login", { state: { from: location } });
+        }
+      });
+    }
+  };
   return (
     <div className="pb-4 bg-slate-100">
       <img className="h-2/4 w-full" src={Image} alt="Shoes" />
@@ -12,7 +60,9 @@ const PopularClassSub = ({ item }) => {
         <p>Available seats:{Available_seats}</p>
         <p>Price ${Price}</p>
       </>
-      <button className="btn btn-sm">Select</button>
+      <button onClick={() => handleSelect(item)} className="btn btn-sm">
+        Select
+      </button>
     </div>
   );
 };
--- src/Components/popularInsSub/PopularInsSub.jsx
+++ src/Components/popularInsSub/PopularInsSub.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 const PopularInsSub = ({ i }) => {
   const { Image, Name, Email } = i;
+
   return (
     <div className=" bg-slate-100">
       <img className="h-2/3 w-full" src={Image} alt="Shoes" />
